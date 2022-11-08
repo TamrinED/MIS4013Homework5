379e5257877e73d1931f6be9e230b22f2d188bb1
TamrinED <98431860+TamrinED@users.noreply.github.com>
2022-11-08 17:25:14 -0600

    Create header.php

---
+++ header.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Homework 4</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    
+ <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="index.php">My Database</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="instructors.php">Instructors</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="courses.php">Courses</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="sections.php">Sections</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="projects.php">Projects</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+    
+  </head>
