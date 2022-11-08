1ec893662aca2e9adad1e1e5d34dab06d6055974
TamrinED <98431860+TamrinED@users.noreply.github.com>
2022-11-08 17:26:09 -0600

    Create handlepost.php

---
+++ handlepost.php
@@ -0,0 +1,11 @@
+<?php require_once("header.php"); ?>
+
+  <h1>Post Response</h1>
+  Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
+  
+  <a class"btn btn-primary" href="instructors.php" role="button">Instructors</a>
+  
+  
+
+<?php require_once("footer.php"); ?>
