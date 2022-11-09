e524a833bdcaaaffec4f7a7c914345a9e05bcbf4
TamrinED <98431860+TamrinED@users.noreply.github.com>
2022-11-09 13:49:58 -0600

    Update courses.php

--- courses.php
+++ courses.php
@@ -99,20 +99,7 @@ if ($result->num_rows > 0) {
       </td>
       
   </tr>
-      <div>
-      <h4> Here's the current syllabuses for these courses</h4>
-         <div> id="flipbook"
-	<div class="hard"> Turn.js </div>
-	<div class="hard">Book of Syllabuses</div>
-	<div> MIS 2113 </div>
-	<div> MIS 3033 </div>
-	<div> MIS 4013 </div>
-	<div> MIS 3353 </div>
-	<div class="hard"></div>
-	<div class="hard"></div>
-</div> 
-          
-      
+         
 <?php
   }
 } else {
