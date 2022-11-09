ff510bfa7424a76440415821d5fa50c38b595bb4
TamrinED <98431860+TamrinED@users.noreply.github.com>
2022-11-09 12:53:38 -0600

    Create jsTesting.php

---
+++ jsTesting.php
@@ -0,0 +1,28 @@
+<?php require_once("header.php") ?>
+
+
+<?php
+function flipbook(){
+<div id="flipbook">
+	<div class="hard"> Turn.js </div>
+	<div class="hard"></div>
+	<div> Page 1 </div>
+	<div> Page 2 </div>
+	<div> Page 3 </div>
+	<div> Page 4 </div>
+	<div class="hard"></div>
+	<div class="hard"></div>
+</div>
+<script type="text/javascript">
+	$("#flipbook").turn({
+		width: 400,
+		height: 300,
+		autoCenter: true
+	});
+</script>
+
+}
+?>
+
+
+<?php require_once("footer.php") ?>
