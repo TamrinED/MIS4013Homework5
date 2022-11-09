2bee74e49ef2e49435f91fd1bb77832ffbe70003
TamrinED <98431860+TamrinED@users.noreply.github.com>
2022-11-09 13:46:12 -0600

    Update jsTesting.php

--- jsTesting.php
+++ jsTesting.php
@@ -1,17 +1,5 @@
 <?php require_once("header.php") ?>
 
-<?php
-
-<div> id="flipbook"
-	<div class="hard"> Turn.js </div>
-	<div class="hard">Book of Syllabuses</div>
-	<div> MIS 2113 </div>
-	<div> MIS 3033 </div>
-	<div> MIS 4013 </div>
-	<div> MIS 3353 </div>
-	<div class="hard"></div>
-	<div class="hard"></div>
-</div>
 <script type="text/javascript">
 	$("#flipbook").turn({
 		width: 400,
@@ -19,8 +7,5 @@
 		autoCenter: true
 	});
 </script>
-
-
-?>
 
 <?php require_once("footer.php") ?>
