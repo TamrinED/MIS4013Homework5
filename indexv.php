053e366105e6369841aee779ada3fa2eb317dff6
TamrinED <98431860+TamrinED@users.noreply.github.com>
2022-11-16 11:39:31 -0600

    Rename index.php to indexv.php

--- indexv.php
+++ indexv.php
@@ -1,8 +1,8 @@
 <?php require_once("header.php"); ?>
 
   <h1>Post Response</h1>
-  Welcome <?php echo $_POST["name"]; ?><br>
-Homework 5: <?php echo $_POST["email"]; ?>
+  Welcome<br>
+Homework 5:
   
   <a class"btn btn-primary" href="homework5.php" role="button">JavaScript Functions</a>
   
