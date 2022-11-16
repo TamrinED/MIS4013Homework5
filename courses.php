da92c6d73a9e0c065270718a66e00e63fea14b1c
TamrinED <98431860+TamrinED@users.noreply.github.com>
2022-11-16 11:28:29 -0600

    Update courses.php

--- courses.php
+++ courses.php
@@ -1,116 +1,6 @@
 <?php require_once("header.php"); ?>
 
-<body>
-    <h1 id = "left">Courses</h1>
-<table class="table table-striped">
-  <thead>
-    <tr>
-      <th>CourseID</th>
-      <th>Prefix</th>
-      <th>Prefix</th>
-      <th>Number</th>
-    </tr>
-  </thead>
-  
-  <tbody>
-    <?php
-$servername = "localhost:3306";
-$username = "tamrined_suser";
-$password = "(_y)XTDI)NmV";
-$dbname = "tamrined_4013Homework3";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-       if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  switch ($_POST['saveType']) {
-    case 'Add':
-      $sqlAdd = "insert into Course (Name) value (?)";
-      $stmtAdd = $conn->prepare($sqlAdd);
-      $stmtAdd->bind_param("s", $_POST['cID']);
-      $stmtAdd->execute();
-      echo '<div class="alert alert-success" role="alert">New course added.</div>';
-      break;
-    case 'Edit':
-      $sqlEdit = "update Course set CourseID=? where CourseID=?";
-      $stmtEdit = $conn->prepare($sqlEdit);
-      $stmtEdit->bind_param("si", $_POST['cID'], $_POST['cid']);
-      $stmtEdit->execute();
-      echo '<div class="alert alert-success" role="alert">Course edited.</div>';
-      break;
-    case 'Delete':
-      $sqlDelete = "delete from Course where ID=?";
-      $stmtDelete = $conn->prepare($sqlDelete);
-      $stmtDelete->bind_param("i", $_POST['cid']);
-      $stmtDelete->execute();
-      echo '<div class="alert alert-success" role="alert">Course deleted.</div>';
-      break;
-  }
-       }
-      
-$sql = "SELECT CourseID, Prefix, Number FROM Course";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-  while($row = $result->fetch_assoc()) {
-      
-      
-?>
-  <tr>
-    <td><?=$row["CourseID"]?></td>
-    <td><?=$row["Prefix"]?></td>
-    <td><?=$row["Number"]?></td>
-      
-    <td>
-              <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#editCourse<?=$row["CourseID"]?>">Edit</button>
-              <div class="modal fade" id="editCourse<?=$row["CourseID"]?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="editCourse<?=$row["CourseID"]?>Label" aria-hidden="true">
-                <div class="modal-dialog">
-                  <div class="modal-content">
-                    <div class="modal-header">
-                      <h1 class="modal-title fs-5" id="editCourse<?=$row["CourseID"]?>Label">Edit Course</h1>
-                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                      <form method="post" action="">
-                        <div class="mb-3">
-                          <label for="editCourse<?=$row["CourseID"]?>ID" class="form-label">Edit CourseID</label>
-                          <input type="text" class="form-control" id="editCourse<?=$row["CourseID"]?>ID" aria-describedby="editCourse<?=$row["CourseID"]?>Help" name="cID" value="<?=$row['CourseID']?>">
-                          <div id="editCourse<?=$row["CourseID"]?>Help" class="form-text">Enter the course's ID.</div>
-                        </div>
-                        <input type="hidden" name="cid" value="<?=$row['CourseID']?>">
-                        <input type="hidden" name="saveType" value="Edit">
-                        <input type="submit" class="btn btn-primary" value="Submit">
-                      </form>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </td>
-      <td>
-           <form method="post" action="">
-                <input type="hidden" name="cid" value="<?=$row["CourseID"]?>" />
-                <input type="hidden" name="saveType" value="Delete">
-                <input type="submit" class="btn" onclick="return confirm('Are you sure you want to delete this course?')" value="Delete">
-              </form>
-      </td>
-      
-      </tr>
-      
-<?php
-  }
-} else {
-  echo "0 results";
-}
-
-$conn->close();
-      
-?>
-  </tbody>
-    </table>
 
 <script>
 function myFunction1() {
@@ -118,8 +8,8 @@ function myFunction1() {
 }
 </script>
 
-<p>When you click "Secret", you will know a secret.</p>
-<p>The secret is a message.</p>
+<p id = left>When you click "Secret", you will know a secret.</p>
+<p id = left>The secret is a message.</p>
 
 <button onclick="myFunction1()">Secret?</button>
 
@@ -132,7 +22,7 @@ function myFunction1() {
            document.getElementById("right").innerHTML = Date();
         }
     </script>
-    <p>Click "Time" to know the current time.</p>
+    <p id = left>Click "Time" to know the current time.</p>
     <button onclick="myFunction2()">Time?</button>
 <p id="right"></p>
 
@@ -143,10 +33,10 @@ function myFunction3() {
 }
 </script>
 
-    <p>Click me to change the color of the title of this webpage</p>
+    <p id = left>Click me to change the color of the title of this webpage</p>
 <button type="button" onclick="myFunction3()">iSpy BLUE!</button><br>
 
-    <p>Who is the better team historically!?</p>
+    <p id = left>Who is the better team historically!?</p>
 
 <script>
 function light(sw) {
